82049fd64deb8ad6ebade031da0dc507067ccee1
Sebas <seplarui@gmail.com>
2019-10-06 12:38:36 +0200

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 'use strict'
 
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 //middlewares -- capas express
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 //peticiones con formato json
 app.use(bodyParser.json());
 
 //RUTAS
 
-app.get ('/api/product', (req,res) => {
+app.get('/api/product', (req, res) => {
+    res.send(200, { products: [] })
+})
+
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({ message: 'El producto se ha recibido' })
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
 
 })
 
 
+//Conect to MONGODB
 
-app.listen(port, () =>  {
-    console.log(`API REST en http://localhost:${port}`);
+mongoose.connection('mongodb://localhost:27017/shop', (err, res) => {
+    if(err) throw err
+    console.log('Conexión a la base de datos establecida');
+
+    app.listen(port, () => {
+        console.log(`API REST en http://localhost:${port}`);
+    })
 })
+
