991208f6965b953dd7849943ae24bf17e71b7376
Sebas <seplarui@gmail.com>
2019-10-09 21:28:16 +0200

    auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+function signUp(req, res) {
+const user = new User({
+    
+})
+}
+
+function signIn(req, res) {
+
+}
+
+module.exports = {
+    signUp,
+    signIn
+}
