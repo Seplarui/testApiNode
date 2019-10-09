f45541bcbc4fcd004a424363b65708e3cf5d6735
Sebas <seplarui@gmail.com>
2019-10-09 13:26:12 +0200

    app.js

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const productCtrl = require ('./controllers/product');
-
-const app = express()
+const app = require('./app');
 const port = process.env.PORT || 3000;
-
-//middlewares -- capas express
-
-app.use(bodyParser.urlencoded({ extended: false }));
-//peticiones con formato json
-app.use(bodyParser.json());
-
-//RUTAS
-
-app.get('/api/product',productCtrl.getProducts);
-
-app.get('/api/product/:productId', productCtrl.getProduct);
-
-app.post('/api/product', productCtrl.saveProduct);
-
-app.put('/api/product/:productId',productCtrl.updateProduct);
-
-app.delete('/api/product/:productId', productCtrl.deleteProduct);
-
 
 //Conect to MONGODB
 
@@ -34,10 +9,7 @@ mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
     if (err) {
         console.log(`Error al conectar con la base de datos: ${err}`);
     }
-
-
     app.listen(port, () => {
         console.log(`API REST en http://localhost:${port}`);
     })
 })
-
