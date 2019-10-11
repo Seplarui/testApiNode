34d598d10473420ee6aa3d1145b8cd48c32f8fce
Sebas <seplarui@gmail.com>
2019-10-12 00:36:38 +0200

    auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,16 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 function signUp(req, res) {
-const user = new User({
-    
-})
+    const user = new User({
+        email: req.body.email,
+        displayName: req.body.displayName
+    })
+
+    user.save((err) => {
+        if (err) res.status(500).send({ message: `Error al creaer el usuario: ${err}` });
+
+        return res.status(200).send({ token: service.createToken(user) })
+    });
 }
 
 function signIn(req, res) {
